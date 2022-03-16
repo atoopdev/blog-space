e955d36fb5e63986cce0ba1592975598ee0630e9
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-16 10:44:12 -0700

    Grab 5 blog posts

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+
+// fetch is by default a GET request
+// can specify method by adding parameter
+fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method:"GET"})
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log(data)
+    // grab first 5 posts
+    const postsArr = data.slice(0,4)
+    console.log(postsArr)
+})
+
+
+// GET - getting data
+// POST - adding new data
+// PUT - Updating existing data
+// DELETE - remove data
