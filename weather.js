781e2a9afece3cefbd8f3b1e6fdab764b367ba66
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-18 13:52:55 -0700

    adjusted search string

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
-fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather?q=seattle")
+// units=imperial converts temps from kelvins to fahrenheit
+fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather?q=seattle&units=imperial")
 .then((response) => response.json())
 .then((json) => console.log("weather: ", json))
